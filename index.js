52a535911d56833e23eb49c892896dbdb024f256
Corals <77341395+Coralise@users.noreply.github.com>
2024-05-04 16:40:15 +0800

    Simple Additions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const firebase = require('./firebase.js')
-const port = 3000
+const port = process.env.PORT || 3000
 
 const app = express()
 
