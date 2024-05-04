27d3e30c12132aa915d6c5b8cbc74185fb7bed6a
Corals <77341395+Coralise@users.noreply.github.com>
2024-05-04 08:48:46 +0800

    More updates

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
-import { getFirestore, collection, getDocs, query, where } from "firebase/firestore";
+import { getFirestore, collection, getDoc, doc, getDocs, query, where, setDoc } from "firebase/firestore";
+import { browserLocalPersistence, createUserWithEmailAndPassword, getAuth, setPersistence, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyBukdTdi8A-hAR8Bp4X-r_FD3UwX50evJk",
   authDomain: "biryani-agbiryani.firebaseapp.com",
@@ -16,6 +17,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const storage = getStorage()
 const db = getFirestore(app)
+const auth = getAuth(app)
+
+setPersistence(auth, browserLocalPersistence)
 
 async function getDishes(options = {}) {
   let dishesColl = collection(db, "dishes")
@@ -28,9 +32,70 @@ async function getDishes(options = {}) {
   return docs.docs.map(e => {return {...e.data(), DocID: e.id}})
 }
 
+async function getCartDishes(cart) {
+  let dishesPromises = Object.keys(cart).map(id => getDoc(doc(db, "dishes", id)))
+  let dishes = (await Promise.all(dishesPromises)).map(e => { return {...e.data(), Count: cart[e.id], DocID: e.id} })
+  return dishes
+}
+
 async function getImageUrl(path) {
   let url = await getDownloadURL(ref(storage, path))
   return url
+}
+
+async function checkVoucherCode(code) {
+  code = code.toUpperCase()
+  let codeDetails = await getDoc(doc(db, "vouchers", code))
+  return codeDetails.data() !== undefined && codeDetails.data().RemainingUses > 0 ? codeDetails.data() : undefined
+}
+
+async function validateVouchers(usedVouchers) {
+  let promisedVouchers = Object.keys(usedVouchers).map(voucher => getDoc(doc(db, "vouchers", voucher)))
+  let vouchers = (await Promise.all(promisedVouchers)).map(e => { return {...e.data(), Code: e.id} })
+  let invalidVouchers = []
+  for (let voucher of vouchers) if (voucher.RemainingUses <= 0) invalidVouchers.push(voucher.Code)
+  return invalidVouchers
+}
+
+function getUser() {
+  return auth.currentUser
+}
+
+async function login(email, pass) {
+  try {
+    let userCreds = await signInWithEmailAndPassword(auth, email, pass)
+    let userDetails = await getDoc(doc(db, "users", email))
+    return {...userCreds, details: userDetails.data()}
+  } catch (e) {
+    return undefined
+  }
+}
+
+async function signup(email, pass, nickname) {
+  try {
+    let userCreds = await createUserWithEmailAndPassword(auth, email, pass)
+
+    await updateProfile(userCreds.user, {
+      displayName: nickname
+    })
+
+    await setDoc(doc(db, "users", email), {
+      Admin: false
+    })
+
+    let userDetails = await getDoc(doc(db, "users", email))
+    return {...userCreds, details: userDetails.data()}
+  } catch (e) {
+    return undefined
+  }
+}
+
+async function logout() {
+  signOut(auth).then(() => {
+    return true
+  }).catch(e => {
+    return false
+  })
 }
 
 export default {
@@ -38,5 +103,12 @@ export default {
     storage,
     db,
     getDishes,
-    getImageUrl
+    getCartDishes,
+    getImageUrl,
+    checkVoucherCode,
+    validateVouchers,
+    getUser,
+    login,
+    signup,
+    logout
 }
--- index.js
+++ index.js
@@ -14,6 +14,18 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/cart', (req, res) => {
+    res.render('view_cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
 app.post('/api/getdishes', async (req, res) => {
     let dishDocs = await firebase.getDishes(req.body)
     let promises = []
@@ -22,6 +34,44 @@ app.post('/api/getdishes', async (req, res) => {
     }
     await Promise.all(promises)
     res.send(dishDocs)
+})
+
+app.post('/api/getcartdishes', async (req, res) => {
+    let dishes = await firebase.getCartDishes(req.body)
+    let promises = []
+    for (const dish of dishes) {
+        promises.push(firebase.getImageUrl(dish.ImageURL).then(url => dish["StorageURL"] = url))
+    }
+    await Promise.all(promises)
+    res.send(dishes)
+})
+
+app.post('/api/checkcode', async (req, res) => {
+    let codeDetails = await firebase.checkVoucherCode(req.body.Code)
+    res.send(codeDetails)
+})
+
+app.post('/api/validatevouchers', async (req, res) => {
+    let codeDetails = await firebase.validateVouchers(req.body)
+    res.send(codeDetails)
+})
+
+app.post('/api/login', async (req, res) => {
+    let userCreds = await firebase.login(req.body.Email, req.body.Password)
+    res.send(userCreds)
+})
+
+app.post('/api/signup', async (req, res) => {
+    let userCreds = await firebase.signup(req.body.Email, req.body.Password, req.body.Nickname)
+    res.send(userCreds)
+})
+
+app.get('/api/getuser', async (req, res) => {
+    res.send(firebase.getUser())
+})
+
+app.get('/api/logout', async (req, res) => {
+    res.send((await firebase.logout()))
 })
 
 app.listen(port, () => {
